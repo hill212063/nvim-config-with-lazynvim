a18cdc5cf349d9eaf3db4c13a936ea15e681d437
hill212063 <hill_sup@hotmail.com>
2024-05-19 09:33:24 +0700

    Config nvim-tree to show file that account in gitignore

--- lua/arepo/plugins/nvim-tree.lua
+++ lua/arepo/plugins/nvim-tree.lua
@@ -1,36 +1,47 @@
 return {
   "nvim-tree/nvim-tree.lua",
- dependencies = "nvim-tree/nvim-web-devicons",  
- config = function()
-  -- disable netrw at the very start of your init.lua
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-  -- optionally enable 24-bit colour
-  vim.opt.termguicolors = true
+    -- optionally enable 24-bit colour
+    vim.opt.termguicolors = true
 
-  -- OR setup with some options
-  require("nvim-tree").setup({
-    sort = {
-      sorter = "case_sensitive",
-    },
-    view = {
-      width = 30,
-    },
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      dotfiles = false,
-    },
-  })
+    -- OR setup with some options
+    require("nvim-tree").setup({
+      update_cwd = true,
+      actions = {
+        open_file = {
+          resize_window = true,
+        },
+      },
+      view = {
+        side = "left",
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      filters = {
+        dotfiles = false,
+      },
+      diagnostics = {
+        enable = true,
+      },
+      git = {
+        enable = true,
+        ignore = false,
+        timeout = 500,
+      },
+    })
+    -- set keymaps
+    local map = vim.api.nvim_set_keymap
 
-  -- set keymaps
-  local map = vim.api.nvim_set_keymap
-
-  map("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-  map("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-  map("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-  map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    map("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })                         -- toggle file explorer
+    map("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    map("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })                     -- collapse file explorer
+    map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })                       -- refresh file explorer
   end,
 }
