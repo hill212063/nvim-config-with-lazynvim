63663104a7e571ab135ad3efe29c514effbbddcb
hill212063 <hill_sup@hotmail.com>
2024-07-07 13:39:09 +0700

    Change theme to be more cozy

--- lua/arepo/plugins/color.lua
+++ lua/arepo/plugins/color.lua
@@ -1,25 +1,8 @@
 return {
-  "daschw/leaf.nvim",
+  "ramojus/mellifluous.nvim",
+  -- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
   config = function()
-    require("leaf").setup({
-      underlineStyle = "underline",
-      commentStyle = "italic",
-      functionStyle = "NONE",
-      keywordStyle = "italic",
-      statementStyle = "bold",
-      typeStyle = "NONE",
-      variablebuiltinStyle = "italic",
-      transparent = true,
-      colors = {},
-      theme = "dark",   -- default, based on vim.o.background, alternatives: "light", "dark"
-      contrast = "low", -- default, alternatives: "medium", "high"
-      overrides = {
-        Comment = { style = "italic" },
-        TelescopeBorder = { link = "Normal" },
-      },
-    })
-
-    -- setup must be called before loading
-    vim.cmd("colorscheme leaf")
+    require("mellifluous").setup({})     -- optional, see configuration section.
+    vim.cmd("colorscheme mellifluous")
   end,
 }
