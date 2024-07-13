c55ac6e6e18169a27295277090304c89eab2dbb4
hill212063 <hill_sup@hotmail.com>
2024-07-13 09:33:21 +0700

    Add projectionist example

--- lua/arepo/plugins/projectionist.lua
+++ lua/arepo/plugins/projectionist.lua
@@ -1,3 +1,21 @@
 return {
 	"tpope/vim-projectionist",
 }
+
+-- Demo java project
+--
+-- name: .projectionist.json
+-- noted: {Test} look like it exclude Test postfix in filename idk why
+--
+--  {
+--   "src/main/java/*.java": {
+--     "alternate": "src/test/java/{}Test.java",
+--     "type": "source"
+--   },
+--   "src/test/java/*.java": {
+--     "alternate": "src/main/java/{}{Test}.java",
+--     "type": "test"
+--   },
+--   "*.java": {"dispatch": "javac {file}"},
+--   "*": {"make": "gradlew"}
+-- }
