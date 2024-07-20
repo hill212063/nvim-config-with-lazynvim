83c6d6907e8f325e75bc6010c53cadd4df5f12a3
hill212063 <hill_sup@hotmail.com>
2024-07-20 22:28:19 +0700

    Remove tabs and change color scheme
    Change color

--- lua/arepo/plugins/color.lua
+++ lua/arepo/plugins/color.lua
@@ -1,14 +1,6 @@
 return {
-  "sekke276/dark_flat.nvim",
+  "EdenEast/nightfox.nvim",
   config = function()
-    require("dark_flat").setup({
-      transparent = true,
-      italics = true
-    })
-
-    -- Line number color
-    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "white", bold = false })
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
-    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "white", bold = false })
+    vim.cmd("colorscheme dayfox")
   end,
 }
