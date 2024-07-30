d3cd9083fc79b1a1ec86cc46eb3bf04908662d20
hill212063 <70126160+hill212063@users.noreply.github.com>
2024-07-30 22:11:49 +0700

    Update telescope-lsp-handlers.lua to depend on telescope

--- lua/arepo/plugins/telescope-lsp-handlers.lua
+++ lua/arepo/plugins/telescope-lsp-handlers.lua
@@ -1,5 +1,8 @@
 return {
   "Slotos/telescope-lsp-handlers.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
   config = function ()
     require'telescope-lsp-handlers'.setup()
   end
