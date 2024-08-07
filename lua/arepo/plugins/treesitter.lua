64ca9b46bd7e60d63b7d6e973b5121e023b9315f
hill212063 <hill_sup@hotmail.com>
2024-08-07 20:58:04 +0700

    Add sql treesitter

--- lua/arepo/plugins/treesitter.lua
+++ lua/arepo/plugins/treesitter.lua
@@ -5,7 +5,20 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "java", "go", "lua", "yaml", "json", "query", "javascript", "typescript", "html", "css", "rust" },
+      ensure_installed = { 
+        "java", 
+        "go", 
+        "lua", 
+        "yaml", 
+        "json", 
+        "query", 
+        "javascript",
+        "typescript",
+        "html", 
+        "css", 
+        "rust",
+        "sql"
+      },
       sync_install = false,
       auto_install = true,
       highlight = { enable = true },
