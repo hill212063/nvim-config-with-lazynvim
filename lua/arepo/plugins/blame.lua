30aebec4fc0f63e9ef09c3f235e998ba50218dd1
hill212063 <hill_sup@hotmail.com>
2024-08-08 22:51:46 +0700

    Remove redundant blame

--- lua/arepo/plugins/blame.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"FabijanZulj/blame.nvim",
-	config = function()
-		require("blame").setup()
-		vim.keymap.set("n", "<leader>tb", vim.cmd.BlameToggle, { window })
-	end,
-}
--- lua/arepo/plugins/git/blame.lua
+++ lua/arepo/plugins/git/blame.lua
@@ -2,5 +2,6 @@ return {
   "FabijanZulj/blame.nvim",
   config = function()
     require("blame").setup()
-  end
+    vim.keymap.set("n", "<leader>tb", vim.cmd.BlameToggle, { desc = "Toggle git blame" })
+  end,
 }
