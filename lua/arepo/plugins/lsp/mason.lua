2bc24bbb2082e5b8a61867672302e0dfd8cbecb4
hill212063 <hill_sup@hotmail.com>
2024-08-10 20:36:28 +0700

    Refactor Mason installed component

--- lua/arepo/plugins/lsp/mason.lua
+++ lua/arepo/plugins/lsp/mason.lua
@@ -1,52 +1,74 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
 
-		local mason_tool_installer = require("mason-tool-installer")
+    local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"pyright",
-				"jsonls",
-				"gopls",
-				"bashls",
-				"dockerls",
-				"terraformls",
-				"docker_compose_language_service",
-				"jdtls",
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        -- LSP
+        "tsserver",
+        "html",
+        "cssls",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "pyright",
+        "jsonls",
+        "gopls",
+        "bashls",
+        "dockerls",
+        "terraformls",
+        "docker_compose_language_service",
+        "jdtls",
         "rust_analyzer",
         "helm_ls",
-				"yamlls",
-			},
-			-- auto-install config server (with lspconfig)
-			automatic_installation = true,
-		})
-	end,
+        "yamlls",
+        -- Formatter
+        "stylua",
+        "prettierd",
+        "xmlformat",
+        "jq",
+        "htmlbeautifier",
+        "yamlfmt",
+        "goimports",
+        "gofmt",
+        "googlejava-format",
+        "rustfmt",
+        "terraform_fmt",
+        "terragrunt_hclfmt",
+        -- Linter
+        "mypy",
+        "ruff",
+        "eslintd",
+        "gitlint",
+        "glint",
+        "gospel",
+        "yamllint"
+      },
+      -- auto-install config server (with lspconfig)
+      automatic_installation = true,
+    })
+  end,
 }
