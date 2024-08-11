1c3267d4a5858fad1b4dd41fad6f392a49875d1e
hill212063 <hill_sup@hotmail.com>
2024-08-11 13:20:53 +0700

    Add file type in dap go

--- lua/arepo/plugins/lsp/go/dap-go.lua
+++ lua/arepo/plugins/lsp/go/dap-go.lua
@@ -1,10 +1,11 @@
 return {
-  "leoluz/nvim-dap-go",
-  dependencies = {
-    "mfussenegger/nvim-dap",
-    "rcarriga/nvim-dap-ui"
-  },
-  config = function()
-    require("dap-go").setup()
-  end
+	"leoluz/nvim-dap-go",
+	ft = "go",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		require("dap-go").setup()
+	end,
 }
