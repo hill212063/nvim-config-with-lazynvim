afa7657bd7026212edb4adffb30139dd6935f12c
hill212063 <hill_sup@hotmail.com>
2024-08-11 13:23:46 +0700

    Add desc for dap

--- lua/arepo/plugins/dap.lua
+++ lua/arepo/plugins/dap.lua
@@ -1,26 +1,26 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "tpope/vim-fugitive",
-    "nvim-neotest/nvim-nio",
-    "folke/trouble.nvim",
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"tpope/vim-fugitive",
+		"nvim-neotest/nvim-nio",
+		"folke/trouble.nvim",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-    dapui.setup()
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-  end,
+		dapui.setup()
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+		vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debug continue" })
+	end,
 }
